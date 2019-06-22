258adc2cb6a3d668627796600d1b5dafe5aed971
Matt Chapman <matt@mttchpmn.com>
2019-06-22 14:54:34 +1200

    Add config and logging

---
+++ config/winston.js
@@ -0,0 +1,45 @@
+const { createLogger, format, transports } = require("winston");
+const appRoot = require("app-root-path");
+
+const options = {
+  file: {
+    level: "info",
+    format: format.combine(
+      format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss"
+      }),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json()
+    ),
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxFiles: 5,
+    colorize: false
+  },
+  console: {
+    level: "debug",
+    format: format.combine(
+      format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss"
+      }),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json()
+    ),
+    handleExceptions: true,
+    json: false,
+    colorize: true
+  }
+};
+
+const logger = createLogger({
+  transports: [
+    new transports.File(options.file),
+    new transports.Console(options.console)
+  ],
+  exitOnError: false
+});
+
+module.exports = logger;
