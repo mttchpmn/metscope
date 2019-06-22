8bc7dc2d3e2939fb48602c624caa7496a3ba5252
Matt Chapman <matt@mttchpmn.com>
2019-06-22 17:37:50 +1200

    Add comments and move routes to sep file

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
+// External imports
 const express = require("express");
 const morgan = require("morgan");
-const pg = require("pg");
 const bodyParser = require("body-parser");
 
+// Internal imports
 const winston = require("./config/winston");
-const scrapeWebcam = require("./controllers/webcam/scrapeWebcam.js");
-const loadWebcam = require("./controllers/webcam/loadWebcam.js");
 
+// Instantiate app
 winston.info(`API starting...`);
 const app = express();
 const port = 3000;
 
+// Setup Middlewares
 app.use(morgan("combined", { stream: winston.stream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ info: `Metscope API online at port ${port}` });
-});
+// Instantiate API endpoints
+require("./config/routes")(app);
 
-app.get("/webcam/scrape/:name", scrapeWebcam);
-app.get("/webcam/load/:name", loadWebcam);
-
-app.listen(port);
+// Launch app
 winston.info(`API online at port ${port}`);
+app.listen(port);
