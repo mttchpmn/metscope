3291526659b2c45c50b3886cfd52a53cf13babce
Matt Chapman <matt@mttchpmn.com>
2019-06-25 13:50:04 +1200

    Implement coronet webcam

---
+++ controllers/webcam/webcam_scrapers/coronetPeak.js
@@ -0,0 +1,29 @@
+const cheerio = require("cheerio");
+
+const pool = require("../../../config/db");
+const winston = require("../../../config/winston");
+const getPage = require("../../../services/retrieveWebPageContent");
+const imageToString = require("../../../services/convertImageToString");
+const saveWebcam = require("../../../services/saveWebcamToDatabase");
+
+module.exports = (req, response) => {
+  winston.info("Scraping Coronet webcam...");
+  const baseUrl = "https://www.coronetpeak.co.nz/weather-report/";
+
+  getPage(baseUrl, 10)
+    .then(res => {
+      let urlRegex = new RegExp(
+        /(\/Webcams\/CoronetPeak\/SugarsRun\/\d{12}\.jpg)+/g
+      );
+
+      let allImages = res.content.match(urlRegex); // Will get all the image URLs from the day
+      const imageUrl = `https://www.coronetpeak.co.nz${allImages.pop()}`;
+
+      imageToString(imageUrl).then(str => {
+        saveWebcam("coronet", imageUrl, str, response);
+      });
+    })
+
+    // TODO - Implement the err from the underlying function
+    .catch(err => winston.error("Failed to get webpage for Coronet"));
+};
--- controllers/webcam/webcam_scrapers/glenorchy.js
+++ controllers/webcam/webcam_scrapers/glenorchy.js
@@ -5,6 +5,7 @@ const saveWebcam = require("../../../services/saveWebcamToDatabase");
 module.exports = (req, res) => {
   const imageUrl = `https://www.snowgrass.co.nz/cust/glenorchy_air/images/webcam.jpg`;
 
+  // TODO - Handle the response seinding in this file, rather than in saveWebcam function
   imageToString(imageUrl).then(string => {
     saveWebcam("glenorchy", imageUrl, string, res);
   });
