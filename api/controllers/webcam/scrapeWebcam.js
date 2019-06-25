5abedde75f264334e97b694deb7bead16f3cc5ba
Matt Chapman <matt@mttchpmn.com>
2019-06-25 23:24:18 +1200

    Implement St Anne webcam

--- api/controllers/webcam/scrapeWebcam.js
+++ api/controllers/webcam/scrapeWebcam.js
@@ -1,12 +1,17 @@
 const winston = require("../../config/winston");
 // Import specific scrapers
-const scrapeGlenorchy = require("./webcam_scrapers/glenorchy");
 const scrapeCoronetPeak = require("./webcam_scrapers/coronetPeak");
+
+const scrapeGlenorchy = require("./webcam_scrapers/glenorchy");
+const scrapeStAnne = require("./webcam_scrapers/stAnne");
+
+const scrapeStatic = require("./webcam_scrapers/static");
 
 // Translate webcam name to scraper method
 const scraperLookup = {
   glenorchy: scrapeGlenorchy,
-  coronet: scrapeCoronetPeak
+  coronet: scrapeCoronetPeak,
+  stAnne: scrapeStAnne
 };
 
 module.exports = (req, res) => {
