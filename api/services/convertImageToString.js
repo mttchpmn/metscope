25287ba2ab92db8cf3523a9a0a0709f378196ecc
Matt Chapman <matt@mttchpmn.com>
2019-06-25 14:22:11 +1200

    Add logging and catch block

--- api/services/convertImageToString.js
+++ api/services/convertImageToString.js
@@ -1,14 +1,20 @@
 const axios = require("axios");
 const axiosRetry = require("axios-retry");
 
+const winston = require("../config/winston");
+
 module.exports = async url => {
   axiosRetry(axios, { retries: 5 });
+  winston.info(`Retrieving image from ${url} - will retry up to 5 times`);
+
   return axios
     .get(url, { responseType: "arraybuffer" })
-    .then(
-      res =>
-        `data:image/png;base64,${Buffer.from(res.data, "binary").toString(
-          "base64"
-        )}`
-    );
+    .then(res => {
+      winston.info("Image retrieved and converted to string");
+
+      return `data:image/png;base64,${Buffer.from(res.data, "binary").toString(
+        "base64"
+      )}`;
+    })
+    .catch(err => winston.error(err));
 };
