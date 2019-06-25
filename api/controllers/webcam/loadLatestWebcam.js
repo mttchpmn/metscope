b2f8aedde6b3a61ef817969ad42fd32bb97ec7a8
Matt Chapman <matt@mttchpmn.com>
2019-06-25 14:29:23 +1200

    Add logging

--- api/controllers/webcam/loadLatestWebcam.js
+++ api/controllers/webcam/loadLatestWebcam.js
@@ -1,13 +1,17 @@
 const pool = require("../../config/db");
+const winston = require("../../config/winston");
 
 const loadLatestWebcam = (req, res) => {
   const webcamName = req.params.name;
+  winston.info(`Loading latest webcam for ${webcamName}`);
+
   pool.query(
     "SELECT id, name, url, string from webcams where name = $1 ORDER BY id DESC LIMIT 1",
     [webcamName],
     (err, results) => {
-      if (err) throw err;
+      if (err) winston.err(err);
 
+      winston.info("Webcam loaded successfully");
       res.set("Content-Type", "text/html");
       res.send(Buffer.from(`<img src="${results.rows[0].string}" />`));
     }
