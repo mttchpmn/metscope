5fe6b6fabdb6a8e6d9b6c7b8f3279c2ee63ab547
Matt Chapman <matt@mttchpmn.com>
2019-06-25 14:59:08 +1200

    Add date when saving img

--- api/services/saveWebcamToDatabase.js
+++ api/services/saveWebcamToDatabase.js
@@ -1,8 +1,12 @@
+const moment = require("moment");
+
 const winston = require("../config/winston");
 const pool = require("../config/db");
 
 module.exports = (name, url, string, response) => {
   winston.info(`Attempting to save ${name} webcam to database`);
+
+  const savedDate = moment.utc().format();
 
   // Load last image from DB to check to see if latest image is the same
   pool
@@ -13,7 +17,7 @@ module.exports = (name, url, string, response) => {
     .then(res => {
       // Don't save the image if it already is in the database
       if (res.rowCount && res.rows[0].string === string) {
-        winston.info(`Image is the same as latest in db`);
+        winston.info(`Image is the same as most recent in database`);
 
         return response.json({
           status: 200,
@@ -23,20 +27,19 @@ module.exports = (name, url, string, response) => {
 
       // Add image to DB if image is new
       pool
-        .query("INSERT INTO webcams (name, url, string) VALUES ($1, $2, $3)", [
-          name,
-          url,
-          string
-        ])
+        .query(
+          "INSERT INTO webcams (name, url, string, date) VALUES ($1, $2, $3, $4)",
+          [name, url, string, savedDate]
+        )
         .then(res => {
-          winston.info(`Image added to db`);
+          winston.info(`Image added to database with date: ${savedDate}`);
           return response.json({
             status: 201,
-            message: "Image added to database"
+            message: `Image added to database with date: ${savedDate}`
           });
         })
         .catch(err => {
-          winston.error(err);
+          // winston.error(err);
           return response.json({ status: 500, message: "Internal error" });
         });
     });
