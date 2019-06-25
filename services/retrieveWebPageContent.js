82520d7fbfc6c05e1b713eece615385af42c2ba5
Matt Chapman <matt@mttchpmn.com>
2019-06-25 13:49:02 +1200

    Add logging and retry functionality

--- services/retrieveWebPageContent.js
+++ services/retrieveWebPageContent.js
@@ -1,10 +1,29 @@
 const axios = require("axios");
+const axiosRetry = require("axios-retry");
 
-module.exports = url => {
-  return axios.get(url).then(res => {
-    return {
-      status: res.status, // Status code e.g. 200
-      content: res.data // Html page content
-    };
-  });
+const winston = require("../config/winston");
+
+module.exports = (url, retryCount) => {
+  winston.info(`Retrieving web page at ${url}`);
+  if (retryCount) {
+    axiosRetry(axios, { retries: retryCount });
+    winston.info(`Will retry up to ${retryCount} times`);
+  }
+
+  return (
+    axios
+      .get(url)
+      .then(res => {
+        winston.info("Request successful");
+        return {
+          status: res.status, // Status code e.g. 200
+          content: res.data // Html page content
+        };
+      })
+      // TODO - Fix why this catch block never seems to get hit
+      .catch(err => {
+        winston.err("Request unsuccessful");
+        return "foo";
+      })
+  );
 };
