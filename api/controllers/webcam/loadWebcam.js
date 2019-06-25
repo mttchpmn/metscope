7ca16ab967495e4736737b8e3978b0be3574a2d4
Matt Chapman <matt@mttchpmn.com>
2019-06-25 14:27:34 +1200

    Add logging

--- api/controllers/webcam/loadWebcam.js
+++ api/controllers/webcam/loadWebcam.js
@@ -1,12 +1,17 @@
 const pool = require("../../config/db");
+const winston = require("../../config/winston");
 
 const loadWebcam = (req, res) => {
   const webcamName = req.params.name;
+  winston.info(`Loading webcams for ${webcamName}`);
+
   pool.query(
     "SELECT id, name, url, string from webcams where name = $1",
     [webcamName],
     (err, results) => {
-      if (err) throw err;
+      if (err) winston.err(err);
+
+      winston.info(`Found ${results.rowCount} rows`);
       res.status(200).json(results.rows);
     }
   );
