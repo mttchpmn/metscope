91d15b52162562586324dcbf439ecf2355ac46f9
Matt Chapman <matt@mttchpmn.com>
2019-06-25 13:48:45 +1200

    Add retry funcitonality and safe Buffer method

--- services/convertImageToString.js
+++ services/convertImageToString.js
@@ -1,11 +1,13 @@
 const axios = require("axios");
+const axiosRetry = require("axios-retry");
 
 module.exports = async url => {
+  axiosRetry(axios, { retries: 5 });
   return axios
     .get(url, { responseType: "arraybuffer" })
     .then(
       res =>
-        `data:image/png;base64,${new Buffer(res.data, "binary").toString(
+        `data:image/png;base64,${Buffer.from(res.data, "binary").toString(
           "base64"
         )}`
     );
