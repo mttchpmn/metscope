2b3841d7dea70e44f09fa868791aa24825d35fa8
Matt Chapman <matt@mttchpmn.com>
2019-06-26 20:07:24 +1200

    Refactor to use promise

--- api/controllers/webcam/loadWebcam.js
+++ api/controllers/webcam/loadWebcam.js
@@ -12,16 +12,16 @@ const loadWebcam = (req, res) => {
     .subtract(24, "hours")
     .format();
 
-  pool.query(
-    "SELECT id, name, date, url, string FROM webcams WHERE name = $1 and date > $2 ORDER BY id DESC",
-    [webcamName, twentyFourHoursAgo],
-    (err, results) => {
-      if (err) winston.err(err);
-
+  pool
+    .query(
+      "SELECT id, name, date, url, string FROM webcams WHERE name = $1 and date > $2 ORDER BY id DESC",
+      [webcamName, twentyFourHoursAgo]
+    )
+    .then(results => {
       winston.info(`Found ${results.rowCount} rows`);
       res.status(200).json(results.rows);
-    }
-  );
+    })
+    .catch(err => winston.error(err));
 };
 
 module.exports = loadWebcam;
