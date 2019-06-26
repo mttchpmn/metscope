a68a2d6f1234a9a6612ce8751f3d89a679823f18
Matt Chapman <matt@mttchpmn.com>
2019-06-26 18:13:02 +1200

    Implement load all

--- api/controllers/webcam/loadAllWebcams.js
+++ api/controllers/webcam/loadAllWebcams.js
@@ -1,12 +1,18 @@
+const moment = require("moment");
+
 const pool = require("../../config/db");
 const winston = require("../../config/winston");
 
-const loadWebcam = (req, res) => {
+const loadWebcam = (req, response) => {
   winston.info(`Loading all webcams`);
 
   //   console.log("req :", req);
   let names;
   let resultObject = {};
+  let twentyFourHoursAgo = moment
+    .utc()
+    .subtract(24, "hours")
+    .format();
 
   // Get all webcam names in database
   pool
@@ -14,6 +20,22 @@ const loadWebcam = (req, res) => {
     .then(results => {
       names = results.rows.map(row => row.name);
       winston.info(`Found all webcam names in database: ${names}`);
+
+      Promise.all(
+        names.map(name =>
+          pool
+            .query(
+              `SELECT  id, name,date, url FROM webcams WHERE name = $1 and date > $2 ORDER BY id DESC`,
+              [name, twentyFourHoursAgo]
+            )
+            .then(results => results.rows)
+        )
+      ).then(res => {
+        names.map((name, index) => {
+          resultObject[name] = res[index];
+        });
+        return response.json(resultObject);
+      });
     })
     .catch(err => winston.error(err));
 };
