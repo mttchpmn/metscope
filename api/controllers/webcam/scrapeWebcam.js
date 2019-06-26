0b614130c7b0c204992a1db82ef851d408b8977f
Matt Chapman <matt@mttchpmn.com>
2019-06-26 12:12:24 +1200

    Implement 404 if webcam doesnt exist

--- api/controllers/webcam/scrapeWebcam.js
+++ api/controllers/webcam/scrapeWebcam.js
@@ -36,6 +36,12 @@ module.exports = (req, res) => {
   winston.info(`Scraping webcam for ${webcamName}`);
 
   // Call scraper method
+  if (!scraperLookup[webcamName])
+    return res.json({
+      status: 404,
+      message: "Webcam scraper not found.  Check your spelling"
+    });
+
   typeof scraperLookup[webcamName] === "string"
     ? scrapeStatic(webcamName, scraperLookup[webcamName], req, res)
     : scraperLookup[webcamName](req, res);
