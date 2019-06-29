87f076620a4576c5b8d6dfacc00c90d03d24553c
Matt Chapman <matt@mttchpmn.com>
2019-06-29 17:43:54 +1200

    Update

--- api/controllers/webcam/purgeWebcams.js
+++ api/controllers/webcam/purgeWebcams.js
@@ -1,5 +1,17 @@
+const moment = require("moment");
+
+const pool = require("../../config/db");
 const winston = require("../../config/winston");
+const webcamList = require("../../config/webcamList");
 
 module.exports = (req, response) => {
   winston.info("Purging webcams");
+
+  let twentyFourHoursAgo = moment.utc().subtract(24, "hours");
+
+  // TODO - iterate through folders and files, check each filename, if old, remove it
+  // After removal, remove entry from DB, or other way round???
+  // Prolly other way round
+  // Should I just add a filename prop to db?  THen when I find old entries, we have the filename to delete directly
+  // YUP, that's it
 };
