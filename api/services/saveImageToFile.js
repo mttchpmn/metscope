e8f78e4867d554f494efbf28de83377806a47397
Matt Chapman <matt@mttchpmn.com>
2019-06-29 18:40:36 +1200

    Refactor to return Promise

--- api/services/saveImageToFile.js
+++ api/services/saveImageToFile.js
@@ -1,38 +1,63 @@
+// External imports
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 const appPath = require("app-root-path");
 
+// Internal imports
+const winston = require("../config/winston");
+
+// Convert fs callback functions to Promises
 const access = util.promisify(fs.access);
 const mkdir = util.promisify(fs.mkdir);
 
-const winston = require("../config/winston");
+// Wrapper function for readability
+const _saveFile = (webcamName, url, fileName) =>
+  new Promise((resolve, reject) => {
+    const writer = fs.createWriteStream(
+      `${appPath}/images/${webcamName}/${fileName}`
+    );
 
-const saveFile = (name, url, fileName) => {
-  const writer = fs.createWriteStream(`${appPath}/images/${name}/${fileName}`);
-
-  axios({ method: "get", url: url, responseType: "stream" }).then(response => {
-    response.data.pipe(writer);
-
-    return new Promise((resolve, reject) => {
-      writer.on("finish", resolve);
-      writer.on("error", reject);
-    });
+    axios({ method: "get", url: url, responseType: "stream" }).then(
+      response => {
+        response.data.pipe(writer);
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+      }
+    );
   });
-};
 
-module.exports = (name, url, fileName) => {
-  winston.info("Saving image to file...");
+module.exports = (webcamName, url, fileName) =>
+  new Promise((resolve, reject) => {
+    winston.info("Saving image to file...");
 
-  access(`${appPath}/images/${name}/`)
-    .then(() => {
-      saveFile(name, url, fileName);
-    })
-    .catch(err => {
-      winston.warn(`${name} directory did not exist.  Creating...`);
+    // Check if directory already exists
+    access(`${appPath}/images/${webcamName}/`)
+      // Dir exists
+      .then(() => {
+        winston.info(`${webcamName} directory exists. Will use.`);
 
-      mkdir(`${appPath}/images/${name}`)
-        .then(() => saveFile(name, url, fileName))
-        .catch(err => winston.error(`ERROR: ${err}`));
-    });
-};
+        _saveFile(webcamName, url, fileName)
+          .then(() => {
+            winston.info("File saved successfully");
+            resolve(fileName);
+          })
+          .catch(() => winston.error("Error saving file"));
+      })
+      // Dir doesn't exist
+      .catch(err => {
+        winston.warn(`${webcamName} directory did not exist.  Creating...`);
+
+        // Create the directory
+        mkdir(`${appPath}/images/${webcamName}`)
+          .then(() => {
+            _saveFile(webcamName, url, fileName)
+              .then(() => {
+                winston.info("File saved successfully");
+                resolve(fileName);
+              })
+              .catch(() => winston.error("Error saving file"));
+          })
+          .catch(err => winston.error(`Error creating directory: ${err}`));
+      });
+  });
