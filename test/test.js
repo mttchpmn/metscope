d811ea0a71732890b718ec247e9456c3f049f8d6
Matt Chapman <matt@mttchpmn.com>
2019-07-10 14:50:04 +1200

    Add tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 const mocha = require("mocha");
 const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 const request = require("supertest");
 const app = require("../app.js");
 const expect = chai.expect;
+
+chai.use(chaiAsPromised);
 
 console.log("Running Tests...");
 
@@ -12,38 +15,75 @@ describe("Example service", () => {
   });
 });
 
-describe("Config and environment", () => {});
+describe("Config and environment", () => {
+  it("Should run tests in test env", () => {
+    expect(process.env.NODE_ENV).to.equal("test");
+  });
+});
 
 describe("API endpoints", () => {
   describe("/webcam", () => {
-    describe("/load/all", () => {});
+    describe("/load/all", () => {
+      it("Should return results in correct format", () => {
+        return request(app)
+          .get("/webcam/load/all")
+          .then(res => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.be.an("object");
+          });
+      });
+    });
 
     describe("/load/latest", () => {});
 
     describe("/load/:name", () => {
-      it("Should return 404 if name not provided", () => {});
-      it("Should return 404 if name not found", () => {});
+      it("Should return 404 if name not provided", () => {
+        return request(app)
+          .get("/webcam/load")
+          .then(res => {
+            expect(res.statusCode).to.equal(404);
+          });
+      });
+
+      it("Should return 404 if name not found", () => {
+        return request(app)
+          .get("/webcam/load/foobarbaz")
+          .then(res => {
+            expect(res.statusCode).to.equal(404);
+          });
+      });
+
       it("Should return results in correct format", () => {
         return request(app)
           .get("/webcam/load/glenorchy")
           .then(res => {
             expect(res.statusCode).to.equal(200);
             expect(res.body).to.be.an("object");
-            expect(res.body).to.have.property("title");
-            expect(res.body).to.have.property("name");
-            expect(res.body).to.have.property("desc");
-            expect(res.body).to.have.property("images");
+            expect(res.body)
+              .to.have.property("title")
+              .and.to.be.a("string");
+            expect(res.body)
+              .to.have.property("name")
+              .and.to.be.a("string");
+            expect(res.body)
+              .to.have.property("desc")
+              .and.to.be.a("string");
+            expect(res.body)
+              .to.have.property("images")
+              .and.to.be.an("array");
           });
       });
-
-      it("Should return object with title property ", () => {});
-      it("Should return object with name property ", () => {});
-      it("Should return object with desc property ", () => {});
-      it("Should return object with images property ", () => {});
-      it("Should return object with images property as array", () => {});
     });
 
-    describe("/purge", () => {});
+    describe("/purge", () => {
+      it("Should purge webcams successfully", () => {
+        return request(app)
+          .get("/webcam/purge")
+          .then(res => {
+            expect(res.statusCode).to.equal(200);
+          });
+      });
+    });
 
     describe("/scrape/:name", () => {});
   });
