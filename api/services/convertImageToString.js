e55c66b6188b51eaea18b38824283d8206d04a33
Matt Chapman <matt@mttchpmn.com>
2019-07-10 13:53:10 +1200

    Convert imageToString service to use promise

--- api/services/convertImageToString.js
+++ api/services/convertImageToString.js
@@ -3,18 +3,20 @@ const axiosRetry = require("axios-retry");
 
 const winston = require("../config/winston");
 
-module.exports = async url => {
-  axiosRetry(axios, { retries: 5 });
-  winston.info(`Retrieving image from ${url} - will retry up to 5 times`);
+module.exports = url =>
+  new Promise((resolve, reject) => {
+    axiosRetry(axios, { retries: 5 });
+    winston.info(`Retrieving image from ${url} - will retry up to 5 times`);
 
-  return axios
-    .get(url, { responseType: "arraybuffer" })
-    .then(res => {
-      winston.info("Image retrieved and converted to string");
-
-      return `data:image/png;base64,${Buffer.from(res.data, "binary").toString(
-        "base64"
-      )}`;
-    })
-    .catch(err => winston.error(err));
-};
+    axios
+      .get(url, { responseType: "arraybuffer" })
+      .then(res => {
+        winston.info("Image retrieved and converted to string");
+        let string = `data:image/png;base64,${Buffer.from(
+          res.data,
+          "binary"
+        ).toString("base64")}`;
+        resolve(string);
+      })
+      .catch(err => reject(err));
+  });
