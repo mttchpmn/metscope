accdacd1c1df7e584ca1e6e59e77d8af0ac0e451
Matt Chapman <matt@mttchpmn.com>
2019-07-24 11:54:25 +1200

    Add lifecycle methods to user model

--- database/models/user.js
+++ database/models/user.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const bcrypt = require("bcryptjs");
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
@@ -12,6 +15,14 @@ module.exports = (sequelize, DataTypes) => {
   );
   User.associate = function(models) {
     // associations can be defined here
+  };
+  User.beforeCreate((user, options) => {
+    return bcrypt
+      .hash(user.password, SALTROUNDS)
+      .then(hashedPassword => (user.password = hashedPassword));
+  });
+  User.validatePassword = function(password, user) {
+    return bcrypt.compare(password, user.password).then(valid => [user, valid]);
   };
   return User;
 };
