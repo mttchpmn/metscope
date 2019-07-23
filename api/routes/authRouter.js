61df6c0d65287104ef9da780e883c298b0a521a1
Matt Chapman <matt@mttchpmn.com>
2019-07-24 11:51:25 +1200

    Add auth routing skeleton:

---
+++ api/routes/authRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/signup", (req, res, next) => {});
+
+router.use("/login", (req, res, next) => {});
+
+router.use("/logout", (req, res, next) => {});
+
+module.exports = router;
