fe0b7c190c5b0d1b27f9e41a83ea12b45a2bea8b
Matt Chapman <matt@mttchpmn.com>
2019-07-23 21:30:48 +1200

    Refactor to use ORM

--- api/controllers/webcam/purge.js
+++ api/controllers/webcam/purge.js
@@ -1,10 +1,13 @@
 const moment = require("moment");
 const fs = require("fs");
 const util = require("util");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 const pool = require("../../config/db");
 const winston = require("../../config/winston");
 const webcamList = require("../../config/webcamList");
+const Webcam = require("../../../database/models").Webcam;
 
 const deleteFile = util.promisify(fs.unlink);
 
@@ -16,32 +19,39 @@ module.exports = (req, response) => {
     .subtract(24, "hours")
     .format();
 
-  pool
-    .query("SELECT id, location FROM webcams WHERE date < $1", [
-      twentyFourHoursAgo
-    ])
-    .then(results => {
+  // pool
+  //   .query("SELECT id, location FROM webcams WHERE date < $1", [
+  //     twentyFourHoursAgo
+  //   ])
+
+  Webcam.findAll({ where: { date: { [Op.gt]: twentyFourHoursAgo } } })
+    .then(webcams => {
       winston.info(
-        `Found ${results.rowCount} rows in the database that will be deleted`
+        `Found ${webcams.length} rows in the database that will be deleted`
       );
-      return results.rows
-        .map(item => item.location)
-        .filter(item => item !== null);
+      return webcams.map(item => item.location).filter(item => item !== null);
     })
     .then(filesToDelete => {
       winston.info("The following files will be deleted:");
       winston.info(filesToDelete);
       return Promise.all(filesToDelete.map(filePath => deleteFile(filePath)));
     })
-    .then(res => {
+    .then(() => {
       winston.info("Files deleted successfully");
-      return pool
-        .query(
-          "DELETE FROM webcams WHERE id IN (SELECT id FROM webcams WHERE date < $1)",
-          [twentyFourHoursAgo]
-        )
-        .then(results => {
-          winston.info(`Deleted ${results.rowCount} rows from the database`);
+      return Webcam.findAll({
+        where: { date: { [Op.lt]: twentyFourHoursAgo } },
+        attributes: ["id"]
+      });
+    })
+    .then(idList => {
+      // return pool
+      //   .query(
+      //     "DELETE FROM webcams WHERE id IN (SELECT id FROM webcams WHERE date < $1)",
+      //     [twentyFourHoursAgo]
+      //   )
+      winston.info(`Will delete ${idList.length} rows from the database`);
+      return Webcam.destroy({ where: { id: idList } })
+        .then(() => {
           return response.json({
             status: 200,
             message: "Webcams purged successfully"
