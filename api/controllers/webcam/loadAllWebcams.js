c065554d761108e3da1a21f7891f47b4f7f5a35f
Matt Chapman <matt@mttchpmn.com>
2019-07-23 20:45:51 +1200

    Refactor to use Sequelise

--- api/controllers/webcam/loadAllWebcams.js
+++ api/controllers/webcam/loadAllWebcams.js
@@ -1,39 +1,30 @@
 const moment = require("moment");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 const pool = require("../../config/db");
 const winston = require("../../config/winston");
 const webcamList = require("../../config/webcamList");
+const Webcam = require("../../../database/models").Webcam;
 
 const loadWebcam = (req, response) => {
   winston.info(`Loading all webcams`);
 
-  let names;
+  const names = Object.keys(webcamList);
   let twentyFourHoursAgo = moment
     .utc()
     .subtract(24, "hours")
     .format();
 
-  // Get all webcam names in database
-  pool
-    .query("SELECT DISTINCT name FROM webcams;")
-    .then(results => {
-      names = results.rows.map(row => row.name).sort();
-      winston.info(`Found all webcam names in database: ${names}`);
-
-      return pool.query(
-        `SELECT  id, name, date, url FROM webcams WHERE date > $1 ORDER BY id ASC;`,
-        [twentyFourHoursAgo]
-      );
-    })
-    .then(results => results.rows)
-    .then(res => {
+  Webcam.findAll({ where: { date: { [Op.gt]: twentyFourHoursAgo } } })
+    .then(webcams => {
       let resultArray = names.map(name => {
         let nameObj = {};
         if (webcamList[name]) {
           nameObj.name = name;
           nameObj.title = webcamList[name].title;
           nameObj.desc = webcamList[name].desc;
-          nameObj.images = res.filter(i => i.name === name); // TODO - Refactor to be more efficient
+          nameObj.images = webcams.filter(i => i.name === name); // TODO - Refactor to be more efficient
         }
         return nameObj;
       });
