a53c17a48002919e04493b71cee07336eef1e26a
Matt Chapman <matt@mttchpmn.com>
2019-07-24 21:45:38 +1200

    Add initial imports and keys to webcams

---
+++ api/config/webcams/clyde.js
@@ -0,0 +1,101 @@
+module.exports = {
+  coronet: {
+    title: "Coronet Top Station",
+    desc: "Situated on Coronet Peak Summit looking SW over Wakatipu Basin",
+    originUrl: "https://www.coronetpeak.co.nz/weather-report/",
+    static: false,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstown" // Do I want to use a key other than 'zone'?
+  },
+  qnAirportNW: {
+    title: "Queenstown Airport NW",
+    desc: "Situated on Queenstown airport looking Northwest towards the tower",
+    static: false,
+    originUrl:
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+  },
+  qnAirportN: {
+    title: "Queenstown Airport N",
+    desc: "Situated on Queenstown airport looking North towards Coronet",
+    static: false,
+    originUrl:
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+  },
+  qnAirportNE: {
+    title: "Queenstown Airport NE",
+    desc: "Situated on Queenstown airport looking NE towards Crown Range",
+    static: false,
+    originUrl:
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+  },
+  qnAirportW: {
+    title: "Queenstown Airport W",
+    desc: "Situated on Queenstown airport looking W down Lake Wakatipu",
+    static: false,
+    originUrl:
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+  },
+  qnAirportE: {
+    title: "Queenstown Airport E",
+    desc: "Situated Queenstown airport looking E towards the Remarkables",
+    static: false,
+    originUrl:
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+  },
+  whareKeaSW: {
+    title: "Whare Kea South-West",
+    desc: "Situated at Whare Kea Chalet looking South-West",
+    static: false,
+    originUrl:
+      "https://www.wharekealodge.com/the-chalet/chalet-webcam-panorama/chalet-webcamsw/"
+  },
+  whareKeaW: {
+    title: "Whare Kea West",
+    desc: "Situated at Whare Kea Chalet looking West",
+    static: false,
+    originUrl:
+      "https://www.wharekealodge.com/the-chalet/chalet-webcam-panorama/chalet-webcamw/"
+  },
+  whareKeaNE: {
+    title: "Whare Kea North-East",
+    desc: "Situated at Whare Kea Chalet looking North-East",
+    static: false,
+    originUrl:
+      "https://www.wharekealodge.com/the-chalet/chalet-webcam-panorama/chalet-webcamne/"
+  },
+  fernhill: {
+    title: "Fernhill",
+    desc: "Situated in Fernhill looking SE towards the Remarkables",
+    static: true,
+    originUrl: "https://www.queenstown.com/cams/aspen.jpg"
+  },
+  cecilPeakW: {
+    title: "Cecil Peak West",
+    desc: "Situated on Cecil Peak looking West",
+    static: true,
+    originUrl:
+      "http://www.jablotool.com/Components/EYE02/StoredImage.ashx?id=QXG2A2FXGT&index=1"
+  },
+  cecilPeakN: {
+    title: "Cecil Peak North",
+    desc: "Situated on Cecil Peak looking North",
+    static: true,
+    originUrl:
+      "http://www.jablotool.com/Components/EYE02/StoredImage.ashx?id=H1H1ISJWPU&index=1"
+  },
+  mtNicholas: {
+    title: "Mt Nicholas",
+    desc: "Situated at Mt Nicholas Station looking NW towards Glenorchy",
+    static: true,
+    originUrl: "http://58.84.48.53:1080/172.23.16.201/cover/1.jpg"
+  },
+  glenorchy: {
+    title: "Glenorchy",
+    desc: "Situated in Glenorchy township looking NW towards Mt Alfred",
+    static: true,
+    originUrl:
+      "https://www.snowgrass.co.nz/cust/glenorchy_air/images/webcam.jpg"
+  }
+};
---
+++ api/config/webcams/index.js
@@ -0,0 +1,9 @@
+const clyde = require("./clyde");
+const fiords = require("./fiords");
+const alps = require("./alps");
+
+module.exports = {
+  clyde,
+  fiords,
+  alps
+};
