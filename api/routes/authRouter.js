41ada617ebf738737b1ec441322602c3e6706a8c
Matt Chapman <matt@mttchpmn.com>
2019-07-24 16:54:47 +1200

    Add logging to authRouter

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -13,6 +13,7 @@ router.post("/signup", (req, res, next) => {
   const password = req.body.password;
 
   if (!firstName || !lastName || !email || !password) {
+    winston.warn(`Missing parameters for user signup`);
     return res.status(422).json({
       message: `Missing parameters. 'firstName', 'lastName', 'email', and 'password' must all be provided.`
     });
@@ -20,12 +21,14 @@ router.post("/signup", (req, res, next) => {
 
   User.findOrCreate({
     where: { email },
-    defaults: { firstName, lastName, email, password }
+    defaults: { firstName, lastName, email, password, tokens: [] }
   })
     .then(([user, userCreated]) => {
       if (userCreated) {
+        winston.info(`New user created for ${user.email}`);
         return res.status(201).json({ user, message: "New user created" });
       }
+      winston.warn(`Email already already in use: ${user.email}`);
       return res.status(400).json({ message: "Email already in use" });
     })
     .catch(error => {
@@ -37,23 +40,46 @@ router.post("/signup", (req, res, next) => {
 router.post("/login", (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  let token;
 
   User.findOne({ where: { email } })
     .then(user => {
       if (!user) {
+        winston.warn(`Email: ${email} not found for user login`);
         return res.status(404).json({ message: "Email not found" });
       }
       return User.validatePassword(password, user);
     })
     .then(([user, valid]) => {
       if (!valid) {
+        winston.warn(`Incorrect password for ${user.email}`);
         return res.status(401).json({ message: "Incorrect password" });
       }
       const payload = { id: user.id, email: user.email };
-      const token = jwt.sign({ user: payload }, config.token.secret, {
+      token = jwt.sign({ user: payload }, config.token.secret, {
         expiresIn: config.token.expiry
       });
-      return res.status(200).json({ token, message: "Logged in successfully" });
+      let newTokenList = [...user.tokens, token];
+
+      console.log("TOKEN ISSUED");
+      console.log(token, "\n\n");
+
+      console.log("user.tokens.length :", user.tokens.length);
+      console.log("newTokenList.length :", newTokenList.length);
+      let check = newTokenList.includes(token);
+      console.log("check :", check);
+
+      return User.update(
+        { tokens: newTokenList },
+        { where: { email: user.email } }
+      );
+    })
+    .then(rowsUpdated => {
+      winston.info(`Log in successful`);
+      return res.status(200).json({
+        token: token,
+        message: "Logged in successfully"
+      });
     })
     .catch(error => {
       winston.error(error.message);
