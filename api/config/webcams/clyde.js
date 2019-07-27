eb63327b87f314ccbacd81020b71667c79b4833f
Matt Chapman <matt@mattchapman.io>
2019-07-27 21:55:15 +1200

    Complete initial Clyde loadout

--- api/config/webcams/clyde.js
+++ api/config/webcams/clyde.js
@@ -1,101 +1,251 @@
-module.exports = {
-  coronet: {
-    title: "Coronet Top Station",
-    desc: "Situated on Coronet Peak Summit looking SW over Wakatipu Basin",
-    originUrl: "https://www.coronetpeak.co.nz/weather-report/",
+"use strict";
+
+// TODO - Add below cams
+// cardrona, treble cone, wanaka lakefront 2, hawea lakefront, wanaka airport, arrowtown(?)
+
+module.exports = [
+  {
+    title: "Queenstown Airport NW",
+    code: "cy_queenstownAirportNW",
+    desc: "Situated on Queenstown airport looking Northwest towards the tower",
+    originUrl:
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam",
     static: false,
     area: "clyde",
     areaCode: "cy",
     region: "otago",
-    zone: "queenstown" // Do I want to use a key other than 'zone'?
+    zone: "queenstownLakes"
   },
-  qnAirportNW: {
-    title: "Queenstown Airport NW",
-    desc: "Situated on Queenstown airport looking Northwest towards the tower",
-    static: false,
-    originUrl:
-      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
-  },
-  qnAirportN: {
+  {
     title: "Queenstown Airport N",
+    code: "cy_queenstownAirportN",
     desc: "Situated on Queenstown airport looking North towards Coronet",
-    static: false,
     originUrl:
-      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam",
+    static: false,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  qnAirportNE: {
+  {
     title: "Queenstown Airport NE",
+    code: "cy_queenstownAirportNE",
     desc: "Situated on Queenstown airport looking NE towards Crown Range",
-    static: false,
     originUrl:
-      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  qnAirportW: {
+  {
     title: "Queenstown Airport W",
+    code: "cy_queenstownAirportW",
     desc: "Situated on Queenstown airport looking W down Lake Wakatipu",
-    static: false,
     originUrl:
-      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  qnAirportE: {
+  {
     title: "Queenstown Airport E",
+    code: "cy_queenstownAirportE",
     desc: "Situated Queenstown airport looking E towards the Remarkables",
-    static: false,
     originUrl:
-      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam"
+      "https://www.queenstownairport.co.nz/travelling/flight-info/webcam",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  whareKeaSW: {
-    title: "Whare Kea South-West",
+  {
+    title: "Whare Kea Chalet Southwest",
+    code: "cy_whareKeaSW",
     desc: "Situated at Whare Kea Chalet looking South-West",
+    originUrl: "URL",
     static: false,
-    originUrl:
-      "https://www.wharekealodge.com/the-chalet/chalet-webcam-panorama/chalet-webcamsw/"
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  whareKeaW: {
-    title: "Whare Kea West",
+  {
+    title: "Whare Kea Chalet West",
+    code: "cy_whareKeaW",
     desc: "Situated at Whare Kea Chalet looking West",
+    originUrl: "URL",
     static: false,
-    originUrl:
-      "https://www.wharekealodge.com/the-chalet/chalet-webcam-panorama/chalet-webcamw/"
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  whareKeaNE: {
-    title: "Whare Kea North-East",
-    desc: "Situated at Whare Kea Chalet looking North-East",
+  {
+    title: "Whare Kea Chalet Northeast",
+    code: "cy_whareKeaNE",
+    desc: "Situated at Whare Kea Chalet looking Northeast",
+    originUrl: "URL",
     static: false,
-    originUrl:
-      "https://www.wharekealodge.com/the-chalet/chalet-webcam-panorama/chalet-webcamne/"
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  fernhill: {
+  {
     title: "Fernhill",
-    desc: "Situated in Fernhill looking SE towards the Remarkables",
+    code: "cy_fernhill",
+    desc:
+      "Situated at Aspen Grove in Fernhill looking Southeast towards the Remarkables",
+    originUrl: "URL",
     static: true,
-    originUrl: "https://www.queenstown.com/cams/aspen.jpg"
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  cecilPeakW: {
+  {
     title: "Cecil Peak West",
+    code: "cy_cecilPeakW",
     desc: "Situated on Cecil Peak looking West",
-    static: true,
     originUrl:
-      "http://www.jablotool.com/Components/EYE02/StoredImage.ashx?id=QXG2A2FXGT&index=1"
+      "http://www.jablotool.com/Components/EYE02/StoredImage.ashx?id=QXG2A2FXGT&index=1",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  cecilPeakN: {
+  {
     title: "Cecil Peak North",
+    code: "cy_cecilPeakW",
     desc: "Situated on Cecil Peak looking North",
-    static: true,
     originUrl:
-      "http://www.jablotool.com/Components/EYE02/StoredImage.ashx?id=H1H1ISJWPU&index=1"
+      "http://www.jablotool.com/Components/EYE02/StoredImage.ashx?id=H1H1ISJWPU&index=1",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  mtNicholas: {
-    title: "Mt Nicholas",
+  {
+    title: "Mt Nicholas Station",
+    code: "cy_mtNicholas",
     desc: "Situated at Mt Nicholas Station looking NW towards Glenorchy",
+    originUrl: "http://58.84.48.53:1080/172.23.16.201/cover/1.jpg",
     static: true,
-    originUrl: "http://58.84.48.53:1080/172.23.16.201/cover/1.jpg"
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   },
-  glenorchy: {
-    title: "Glenorchy",
+  {
+    title: "Glenorchy Township",
+    code: "cy_glenorchyTownship",
     desc: "Situated in Glenorchy township looking NW towards Mt Alfred",
-    static: true,
     originUrl:
-      "https://www.snowgrass.co.nz/cust/glenorchy_air/images/webcam.jpg"
+      "https://www.snowgrass.co.nz/cust/glenorchy_air/images/webcam.jpg",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Pisa Range",
+    code: "cy_pisaRange",
+    desc: "Situated at the Snow Farm looking South down the Pisa Range",
+    originUrl: "http://snowgrass.co.nz/cust/snow_farm/images/webcam.jpg",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Queenstown Basin",
+    code: "cy_queenstownBasin",
+    desc:
+      "Situated on the Crown Terrace looking West across the Wakatipu basin",
+    originUrl:
+      "http://icons.wunderground.com/webcamramdisk/l/i/Live24NZ/4/current.jpg?1544682451",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Cromwell Township",
+    code: "cy_cromwellTownship",
+    desc: "Situated in the Cromwell CBD looking West towards the Pisa Range",
+    originUrl: "http://snowgrass.co.nz/cust/cromwell/images/webcam.jpg",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Cromwell Old Town",
+    code: "cy_cromwellOldTown",
+    desc: "Situated at the Old Cromwell town, looking West",
+    originUrl:
+      "http://snowgrass.co.nz/cust/cromwell//images/webcam_heritage_1080.jpg",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Cromwell Vineyards",
+    code: "cy_cromwellVineyards",
+    desc: "Situated at Scott Base Vineyards, looking East",
+    originUrl: "URL",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Lake Dunstan",
+    code: "cy_lakeDunstan",
+    desc: "Situated at Clyde Dam looking Northwest over Lake Dunstan",
+    originUrl: "http://snowgrass.co.nz/cust/contact/clyde/index.html",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Wanaka Lakefront",
+    code: "cy_wanakaLakefront",
+    desc: "Situated at Wanaka Lakefront, looking Northwest over Lake Wanaka",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/t2c2fbf7c29w201.jpg?hide=1",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
+  },
+  {
+    title: "Alexandra Township",
+    code: "cy_alexandraTownship",
+    desc: "Situated at Alexandra Township, looking Northwest",
+    originUrl: "https://alexandra.co.nz/JPG/image.jpg",
+    static: true,
+    area: "clyde",
+    areaCode: "cy",
+    region: "otago",
+    zone: "queenstownLakes"
   }
-};
+];
