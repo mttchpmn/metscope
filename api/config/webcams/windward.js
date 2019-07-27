78f4b3cc6ca1e7564b927b8ae16bc2d3d14ad3a8
Matt Chapman <matt@mttchpmn.com>
2019-07-27 19:10:58 +1200

    Add POC

---
+++ api/config/webcams/windward.js
@@ -0,0 +1,87 @@
+module.exports = [
+  {
+    title: "Greymouth Township",
+    desc: "Situated looking West over the township of Greymouth",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/tc80543a0f8w26.jpg?hide=1",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "greymouth"
+  },
+  {
+    title: "Fox Glacier",
+    desc: "DESC",
+    originUrl:
+      "http://www.snowgrass.co.nz/cust/fox_glacier/fox_images/webcam.jpg",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
+    title: "Franz Josef Glacier",
+    desc: "DESC",
+    originUrl: "http://www.snowgrass.co.nz/franz/cam6.jpg",
+    static: false,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
+    title: "Franz Josef Township - North",
+    desc: "DESC",
+    originUrl: "https://www.trafficnz.info/camera/664.jpg",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
+    title: "Fox Glacier Township - North",
+    desc: "DESC",
+    originUrl: "https://www.trafficnz.info/camera/662.jpg",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
+    title: "High Peaks Hotel",
+    desc: "DESC",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/t666d557294w45.jpg?hide=1",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
+    title: "Greymouth Aero Club",
+    desc: "DESC",
+    originUrl:
+      "http://210.54.120.112:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=visitor&pwd=abc123",
+    static: false,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "greymouth"
+  },
+  {
+    title: "Haast Pass",
+    desc:
+      "Situated at Pleasant Flat in Haast Pass, lookin NE towards the Landsborough river",
+    originUrl: "https://www.trafficnz.info/camera/738.jpg",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  }
+];
