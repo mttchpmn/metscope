4e0b865ace999a1968c80efe9b4afb7d9ac172bb
Matt Chapman <matt@mattchapman.io>
2019-07-27 19:24:46 +1200

    Finalise windward config

--- api/config/webcams/windward.js
+++ api/config/webcams/windward.js
@@ -1,18 +1,21 @@
 module.exports = [
   {
-    title: "Greymouth Township",
-    desc: "Situated looking West over the township of Greymouth",
-    originUrl:
-      "https://encounter.snapithd.com/static_new/tc80543a0f8w26.jpg?hide=1",
+    title: "Haast Pass",
+    code: "ww_haastPass",
+    desc:
+      "Situated at Pleasant Flat in Haast Pass, lookin NE towards the Landsborough river",
+    originUrl: "https://www.trafficnz.info/camera/738.jpg",
     static: true,
     area: "windward",
     areaCode: "ww",
     region: "westCoast",
-    zone: "greymouth"
+    zone: "westland"
   },
   {
     title: "Fox Glacier",
-    desc: "DESC",
+    code: "ww_foxGlacier",
+    desc:
+      "Situated on a hill to the West of the glacier, looking towards the glacier itself",
     originUrl:
       "http://www.snowgrass.co.nz/cust/fox_glacier/fox_images/webcam.jpg",
     static: true,
@@ -22,28 +25,9 @@ module.exports = [
     zone: "westland"
   },
   {
-    title: "Franz Josef Glacier",
-    desc: "DESC",
-    originUrl: "http://www.snowgrass.co.nz/franz/cam6.jpg",
-    static: false,
-    area: "windward",
-    areaCode: "ww",
-    region: "westCoast",
-    zone: "westland"
-  },
-  {
-    title: "Franz Josef Township - North",
-    desc: "DESC",
-    originUrl: "https://www.trafficnz.info/camera/664.jpg",
-    static: true,
-    area: "windward",
-    areaCode: "ww",
-    region: "westCoast",
-    zone: "westland"
-  },
-  {
-    title: "Fox Glacier Township - North",
-    desc: "DESC",
+    title: "Fox Township - North",
+    code: "ww_foxTownshipN",
+    desc: "Situated at Fox township, looking North",
     originUrl: "https://www.trafficnz.info/camera/662.jpg",
     static: true,
     area: "windward",
@@ -53,7 +37,8 @@ module.exports = [
   },
   {
     title: "High Peaks Hotel",
-    desc: "DESC",
+    code: "ww_highPeaksHotel",
+    desc: "Situated at High Peaks Hotel, 2km West of Fox township",
     originUrl:
       "https://encounter.snapithd.com/static_new/t666d557294w45.jpg?hide=1",
     static: true,
@@ -63,8 +48,31 @@ module.exports = [
     zone: "westland"
   },
   {
+    title: "Franz Josef Glacier",
+    code: "ww_franzJosefGlacier",
+    desc: "Situated at Franz Josef Glacier, looking West towards the coast",
+    originUrl: "http://www.snowgrass.co.nz/franz/cam6.jpg",
+    static: false,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
+    title: "Franz Josef Township - North",
+    code: "ww_franzJosefTownshipN",
+    desc: "Situated at Franz Josef township, looking North",
+    originUrl: "https://www.trafficnz.info/camera/664.jpg",
+    static: true,
+    area: "windward",
+    areaCode: "ww",
+    region: "westCoast",
+    zone: "westland"
+  },
+  {
     title: "Greymouth Aero Club",
-    desc: "DESC",
+    code: "ww_greymouthAirport",
+    desc: "Situated at Greymouth Airport looking Southeast",
     originUrl:
       "http://210.54.120.112:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=visitor&pwd=abc123",
     static: false,
@@ -74,14 +82,15 @@ module.exports = [
     zone: "greymouth"
   },
   {
-    title: "Haast Pass",
-    desc:
-      "Situated at Pleasant Flat in Haast Pass, lookin NE towards the Landsborough river",
-    originUrl: "https://www.trafficnz.info/camera/738.jpg",
+    title: "Greymouth Township",
+    code: "ww_greymouthTownship",
+    desc: "Situated looking West over the township of Greymouth",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/tc80543a0f8w26.jpg?hide=1",
     static: true,
     area: "windward",
     areaCode: "ww",
     region: "westCoast",
-    zone: "westland"
+    zone: "greymouth"
   }
 ];
