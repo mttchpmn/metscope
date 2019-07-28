826560b3221d5db638f95e7a5f6ac23b83f69baf
Matt Chapman <matt@mattchapman.io>
2019-07-28 18:17:53 +1200

    Update scrape all controller

--- api/controllers/webcam/scrapeAll.js
+++ api/controllers/webcam/scrapeAll.js
@@ -1,29 +1,24 @@
+"use strict";
+
+const allSettled = require("promise.allsettled");
+
 const winston = require("../../config/winston");
-// Import specific scrapers
-const scrapeCoronetPeak = require("./scrapers/coronetPeak");
-const scrapeWhareKeaSW = require("./scrapers/whareKeaSW");
-const scrapeWhareKeaW = require("./scrapers/whareKeaW");
-const scrapeWhareKeaNE = require("./scrapers/whareKeaNE");
-const scrapeQueenstownAirport = require("./scrapers/queenstownAirport");
 const scrapeStatic = require("./scrapers/_static");
 
 const webcamList = require("../../config/webcams").clyde;
 
-const allSettled = require("promise.allsettled");
-
-// USE PROMISE.all and refactor scraper methods
-// Errors are occurring (status and json = undefined) as we are not passing reponse along
-
 module.exports = (req, res) => {
   winston.info("Scraping all webcams");
 
-  // Promise.all method (Throwing an error - not sure why)
   const promises = webcamList
-    .filter(cam => cam.static)
+    // .filter(cam => cam.static)
     .map(cam => {
       if (cam.static) return scrapeStatic(cam.code, cam.originUrl);
-      return;
+      let dynamicScraper = require(`./scrapers/${cam.code}`);
+      console.log("Here");
+      return dynamicScraper(cam.code, cam.originUrl);
     });
+
   allSettled(promises)
     .then(response => {
       winston.info("Scraping successful");
