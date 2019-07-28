2e90a57f948806329249cf3f019964d1272995ab
Matt Chapman <matt@mattchapman.io>
2019-07-28 19:34:22 +1200

    Update scrape controller

--- api/controllers/webcam/scrape.js
+++ api/controllers/webcam/scrape.js
@@ -1,51 +1,45 @@
 "use strict";
 
 const winston = require("../../config/winston");
-const webcamList = require("../../config/webcamList");
-// Import specific scrapers
-const scrapeCoronetPeak = require("./scrapers/coronetPeak");
-const scrapeWhareKeaSW = require("./scrapers/whareKeaSW");
-const scrapeWhareKeaW = require("./scrapers/whareKeaW");
-const scrapeWhareKeaNE = require("./scrapers/whareKeaNE");
-const scrapeQueenstownAirport = require("./scrapers/queenstownAirport");
-const scrapeStatic = require("./scrapers/static");
-
-// TODO - Better webcam naming conventions
-
-// Translate webcam name to scraper method
-const dynamicScrapers = {
-  coronet: scrapeCoronetPeak,
-  whareKeaSW: scrapeWhareKeaSW,
-  whareKeaW: scrapeWhareKeaW,
-  whareKeaNE: scrapeWhareKeaNE,
-  qnAirportW: scrapeQueenstownAirport,
-  qnAirportNW: scrapeQueenstownAirport,
-  qnAirportN: scrapeQueenstownAirport,
-  qnAirportNE: scrapeQueenstownAirport,
-  qnAirportE: scrapeQueenstownAirport
-};
+const webcamList = require("../../config/webcams").clyde;
+const scrapeStatic = require("./scrapers/_static");
 
 module.exports = (req, res) => {
   const webcamName = req.params.name;
   winston.info(`Scraping webcam for ${webcamName}`);
 
+  const allCamNames = webcamList.map(cam => cam.code);
+  const requestedCam = webcamList.filter(cam => cam.code === webcamName)[0];
+  console.log("requestedCam :", requestedCam);
+  console.log("allCamNames :", allCamNames);
+
   // Return 404 if we can't find the requested webcam
-  if (!webcamList[webcamName]) {
+  if (!allCamNames.includes(webcamName)) {
     winston.warn(`Scraper for webcam: ${webcamName} not found`);
     return res.json({
       status: 404,
-      message: `Webcam scraper not found.  Available webcams are: ${Object.keys(
-        scraperLookup
-      )}`
+      message: `Webcam scraper not found.  Available webcams are: ${allCamNames}`
     });
   }
 
-  webcamList[webcamName].static
-    ? scrapeStatic(webcamName, webcamList[webcamName].originUrl, req, res)
-    : dynamicScrapers[webcamName](
-        webcamName,
-        webcamList[webcamName].originUrl,
-        req,
-        res
-      );
+  if (requestedCam.static === false) {
+    const scrapeDynamic = require(`./scrapers/${webcamName}`);
+    scrapeDynamic(webcamName, requestedCam.originUrl)
+      .then(({ code, newImage }) =>
+        res.status(200).json({ message: "Scrape successful", code, newImage })
+      )
+      .catch(err => {
+        winston.error(err.message);
+        res.status(500).json({ message: "Internal error" });
+      });
+  }
+  console.log("requestedCam.originUrl :", requestedCam);
+  scrapeStatic(webcamName, requestedCam.originUrl)
+    .then(({ code, newImage }) =>
+      res.status(200).json({ message: "Scrape successful", code, newImage })
+    )
+    .catch(err => {
+      winston.error(err.message);
+      res.status(500).json({ message: "Internal error" });
+    });
 };
