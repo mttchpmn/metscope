9ba35f3864423533110fed1b5c09d8f0062f4f55
Matt Chapman <matt@mattchapman.io>
2019-07-28 20:45:22 +1200

    Update load/all func

--- api/controllers/webcam/loadAll.js
+++ api/controllers/webcam/loadAll.js
@@ -5,13 +5,13 @@ const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
 const winston = require("../../config/winston");
-const webcamList = require("../../config/webcamList");
+const webcamList = require("../../config/webcams").all;
 const Webcam = require("../../../database/models").Webcam;
 
-const loadWebcam = (req, response) => {
+const loadWebcam = (req, res) => {
   winston.info(`Loading all webcams`);
 
-  const names = Object.keys(webcamList);
+  const allNames = webcamList.map(cam => cam.code);
   let twentyFourHoursAgo = moment
     .utc()
     .subtract(24, "hours")
@@ -19,19 +19,19 @@ const loadWebcam = (req, response) => {
 
   Webcam.findAll({ where: { date: { [Op.gt]: twentyFourHoursAgo } } })
     .then(webcams => {
-      let resultArray = names.map(name => {
-        let nameObj = {};
-        if (webcamList[name]) {
-          nameObj.name = name;
-          nameObj.title = webcamList[name].title;
-          nameObj.desc = webcamList[name].desc;
-          nameObj.images = webcams.filter(i => i.name === name); // TODO - Refactor to be more efficient
-        }
-        return nameObj;
+      let resultArray = webcamList.map(cam => {
+        cam.images = webcams.filter(i => i.name === cam.code);
+        delete cam.originUrl;
+        delete cam.static;
+        return cam;
       });
-      return response.json(resultArray);
+
+      return res.status(200).json(resultArray);
     })
-    .catch(err => winston.error(err));
+    .catch(err => {
+      winston.error(err.message);
+      return res.status(500).json({ message: "Internal error" });
+    });
 };
 
 module.exports = loadWebcam;
