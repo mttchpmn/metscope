9dddfa0d4947f4528a5d0b02b84c1ac9b67bdfc0
Matt Chapman <matt@mattchapman.io>
2019-07-29 18:58:34 +1200

    Refactor response

--- api/controllers/webcam/loadAll.js
+++ api/controllers/webcam/loadAll.js
@@ -1,37 +1,50 @@
 "use strict";
 
+// External Imports
 const moment = require("moment");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
+// Internal Imports
 const winston = require("../../config/winston");
-const webcamList = require("../../config/webcams").all;
+const webcamIndex = require("../../config/webcams");
 const Webcam = require("../../../database/models").Webcam;
 
-const loadWebcam = (req, res) => {
+const loadAllWebcams = (req, res) => {
   winston.info(`Loading all webcams`);
 
-  const allNames = webcamList.map(cam => cam.code);
-  let twentyFourHoursAgo = moment
+  const twentyFourHoursAgo = moment
     .utc()
     .subtract(24, "hours")
     .format();
 
+  const data = {
+    webcams: {}
+  };
+
+  // Find all webcams in database from the last 24 hrs
   Webcam.findAll({ where: { date: { [Op.gt]: twentyFourHoursAgo } } })
     .then(webcams => {
-      let resultArray = webcamList.map(cam => {
-        cam.images = webcams.filter(i => i.name === cam.code);
-        delete cam.originUrl;
-        delete cam.static;
-        return cam;
+      // Comment out this line to add 'all' as a prop to the repsonse, which contains all cams
+      // This DRAMATICALLY increases the number of lines in the response
+      delete webcamIndex.all;
+
+      Object.keys(webcamIndex).map(area => {
+        data.webcams[area] = webcamIndex[area];
+
+        data.webcams[area].map(cam => {
+          delete cam.originUrl;
+          delete cam.static;
+          cam.images = webcams.filter(i => i.name === cam.code);
+        });
       });
 
-      return res.status(200).json(resultArray);
+      return res.status(200).json({ data });
     })
     .catch(err => {
       winston.error(err.message);
-      return res.status(500).json({ message: "Internal error" });
+      return res.status(500).json({ error: "Internal error" });
     });
 };
 
-module.exports = loadWebcam;
+module.exports = loadAllWebcams;
