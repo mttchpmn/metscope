8b7cda8ba58f8d7ad954877bcdcca2a9bdedf5e4
Matt Chapman <matt@mattchapman.io>
2019-07-29 17:19:40 +1200

    Add all gore webcams

---
+++ api/config/webcams/gore.js
@@ -0,0 +1,113 @@
+module.exports = [
+  {
+    title: "Waikouaiti",
+    code: "ge_WaikouaitiTownship",
+    desc: "Situated at Waikouaiti township, looking North",
+    originUrl: "http://media.wickednetworks.co.nz/current-waikouaiti.jpg",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "dunedin"
+  },
+  //   {
+  //     title: "Mt Cargill",
+  //     code: "ge_mtCargill",
+  //     desc: "Situated at the Leith Saddle on Mt Cargill, looking West",
+  //     originUrl: "https://www.metservice.com/national/traffic-cams/leith-saddle",
+  //     static: false,
+  //     area: "gore",
+  //     areaCode: "ge",
+  //     region: "otago",
+  //     zone: "southEastCoast"
+  //   },
+  {
+    title: "Dunedin City",
+    code: "ge_dunedinCity",
+    desc: "Situated in Dunedin City, looking Northeast towards Mt Cargill",
+    originUrl: "http://nzmoray.co.nz/webcam/cam-image00001.jpg",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "dunedin"
+  },
+  {
+    title: "North East Valley",
+    code: "ge_northEastValley",
+    desc: "Situated looking Northwest over Dunedin's Northeast Valley",
+    originUrl: "http://www.hyland.net.nz/oldsite/iphone.jpg",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "dunedin"
+  },
+  {
+    title: "St Clair Beach",
+    code: "ge_stClairBeach",
+    desc: "Situated at St Clair beach, looking East",
+    originUrl: "http://media.wickednetworks.co.nz/current-stclair.jpg",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "dunedin"
+  },
+  //   {
+  //     title: "Port Chalmers",
+  //     code: "ge_portChalmers",
+  //     desc: "Situated at the Port Chalmers container terminal, looking Northeast",
+  //     originUrl: "https://www.portotago.co.nz/otago-harbour/webcams/",
+  //     static: true,
+  //     area: "gore",
+  //     areaCode: "ge",
+  //     region: "otago",
+  //     zone: "dunedin"
+  //   },
+  //   {
+  //     title: "Taiaroa Head",
+  //     code: "ge_taiaroaHead",
+  //     desc: "Situated at Taiaroa Head, looking Northwest towards Aramoana",
+  //     originUrl: "https://www.portotago.co.nz/otago-harbour/webcams/",
+  //     static: true,
+  //     area: "gore",
+  //     areaCode: "ge",
+  //     region: "otago",
+  //     zone: "dunedin"
+  //   },
+  {
+    title: "Stewart Island Northwest",
+    code: "ge_stewartIslandNW",
+    desc: "Situated at Oban township on Stewart Island, looking Northwest",
+    originUrl: "https://www.sailsashore.co.nz/flights/capture1.jpg",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "stewartIsland"
+  },
+  {
+    title: "Stewart Island North",
+    code: "ge_stewartIslandN",
+    desc: "Situated at Oban township on Stewart Island, looking North",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/t596b33459cw95.jpg?hide=1",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "stewartIsland"
+  },
+  {
+    title: "Stewart Island Northeast",
+    code: "ge_stewartIslandNE",
+    desc: "Situated at Oban township on Stewart Island, looking Northeast",
+    originUrl: "https://www.sailsashore.co.nz/image/cam/snapshot1.jpg",
+    static: true,
+    area: "gore",
+    areaCode: "ge",
+    region: "otago",
+    zone: "stewartIsland"
+  }
+];
