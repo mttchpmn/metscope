75130c892da36d021d9e0cddb4c7625b0666fb20
Matt Chapman <matt@mattchapman.io>
2019-07-29 16:58:25 +1200

    Add all fiords webcams
    :

--- api/config/webcams/fiords.js
+++ api/config/webcams/fiords.js
@@ -1,1 +1,106 @@
-module.exports = [];
+module.exports = [
+  {
+    title: "Homer Tunnel",
+    code: "fd_homerTunnel",
+    desc:
+      "Situated in the mid Cleddau Valley, looking South.  (The Homer Tunnel is around the corner to the left)",
+    originUrl: "https://metdata.net.nz/SH94/camera/41/image.php",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "MacKinnon Pass",
+    code: "fd_mackinnonPass",
+    desc:
+      "Situated on Mackinnon Pass looking down the West Branch of the Clinton River",
+    originUrl: "https://metdata.net.nz/doc/mackinnon/cam1/image.php",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "St Anne Point",
+    code: "fd_stAnnePoint",
+    desc: "Situated at St Anne Point looking North-East",
+    originUrl: "https://metdata.net.nz/es/stanne/cam1/image.php",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "Milford Sound (Real Journeys Dock)",
+    code: "fd_milfordSoundRJ",
+    desc: "Situated at the Real Journeys Dock looking towards Mitre Peak",
+    originUrl: "http://snapithd.com/static/milford.jpg",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "Milford Sound (Southern Discoveries Dock)",
+    code: "fd_milfordSoundSD",
+    desc:
+      "Situated at the Southern Discoveries Dock looking towards Mitre Peak",
+    originUrl:
+      "https://www.southerndiscoveries.co.nz/webcam-images/terminal/webcam1.jpg",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "Milford Sound Southwest",
+    code: "fd_milfordSoundSW",
+    desc: "Situated at the Real Journeys office looking South-West",
+    originUrl:
+      "http://webcam.realjourneys.co.nz/watermark.php?source=mfdwebcam3.jpg",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "Te Anau Airport West",
+    code: "fd_teAnauAirportW",
+    desc: "Situated at Te Anau Airport, looking West",
+    originUrl: "http://www.teanauairport.co.nz/images/west.jpg",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "Te Anau Airport North",
+    code: "fd_teAnauAirportN",
+    desc: "Situated at Te Anau Airport, looking North",
+    originUrl: "http://www.teanauairport.co.nz/images/apron.jpg",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  },
+  {
+    title: "Kepler Track",
+    code: "fd_keplerTrack",
+    desc: "Situated on the Kepler Track",
+    originUrl: "https://www.metdata.net.nz/doc/kepler/cam2/image.php",
+    static: true,
+    area: "fiords",
+    areaCode: "fd",
+    region: "southland",
+    zone: "fiordland"
+  }
+];
