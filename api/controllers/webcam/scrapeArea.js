2589e84560095e73e12f614f255bf55efbc3bfe6
Matt Chapman <matt@mttchpmn.com>
2020-02-26 22:06:29 +1300

    Tidy up reponsone

--- api/controllers/webcam/scrapeArea.js
+++ api/controllers/webcam/scrapeArea.js
@@ -11,28 +11,19 @@ module.exports = async (req, res) => {
   const areaWebcams = webcams[req.params.area];
 
   try {
-    const result = await Promise.all(
+    await Promise.all(
       areaWebcams.map(cam => {
         // Dynamic cam
         if (!cam.static) {
-          // winston.debug(`[${cam.code}]: Static: ${cam.static}`);
-          // winston.debug(`Dynamic: ${JSON.stringify(cam)}`);
-          // winston.debug(`[${cam.code}]: Cam is dynamic.  Skipping...`);
-          return "SKIPPED DYNAMIC SCRAPER";
+          return;
         }
 
         // Static cam
-        // winston.debug(`[${cam.code}]: Static: ${cam.static}`);
-        // winston.debug(`Static: ${JSON.stringify(cam)}`);
         return processWebcam(cam.code, cam.originUrl);
       })
     );
-    winston.info(
-      `Successfully scraped webcams for ${req.params.area}: ${JSON.stringify(
-        result
-      )}`
-    );
-    return res.status(200).json({ message: "Scrape successful", data: result });
+    winston.info(`Successfully scraped webcams for ${req.params.area}`);
+    return res.status(200).json({ message: "Scrape successful" });
   } catch (error) {
     winston.error(`Error: ${error}`);
     return res.status(500).json({ error: "Internal error", data: error });
