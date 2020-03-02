046d91dd4383b0a73006ecc538d6817b900d0eb0
Matt Chapman <matt@mttchpmn.com>
2020-03-02 19:14:27 +1300

    Add execution time

--- api/controllers/webcam/scrape.js
+++ api/controllers/webcam/scrape.js
@@ -5,6 +5,7 @@ const webcamList = require("../../../config/webcams");
 const processWebcam = require("../../helpers/webcam/processWebcam");
 
 module.exports = async (req, res) => {
+  const startTime = new Date();
   winston.info(`Scraping webcams for ${req.params.area}`);
   const webcams = webcamList[req.params.area];
 
@@ -20,11 +21,16 @@ module.exports = async (req, res) => {
       .filter(([code, successful]) => !successful)
       .map(([code, successful]) => code);
 
+    const endTime = new Date();
+    const executionTime = (endTime - startTime) / 1000;
     winston.info(`Successfully scraped webcams for ${req.params.area}`);
 
     return res
       .status(200)
-      .json({ message: "Scrape successful", data: { successes, failures } });
+      .json({
+        message: "Scrape successful",
+        data: { executionTime, successes, failures }
+      });
   } catch (error) {
     winston.error(`Error: ${error}`);
 
