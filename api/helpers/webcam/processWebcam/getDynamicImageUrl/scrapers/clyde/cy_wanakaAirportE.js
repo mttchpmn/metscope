86ef93d95926a3eb080b49792c6a54120a55f5f3
Matt Chapman <matt@mttchpmn.com>
2020-03-04 13:07:08 +1300

    Add Wanaka AIrport scrapers

---
+++ api/helpers/webcam/processWebcam/getDynamicImageUrl/scrapers/clyde/cy_wanakaAirportE.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const cheerio = require("cheerio");
+
+const getWebContent = require("../../../../../util/getWebContent");
+
+module.exports = async originUrl => {
+  let imageUrl;
+
+  try {
+    const html = await getWebContent(originUrl);
+    const $ = cheerio.load(html);
+    $("img").map((i, elem) => {
+      if (elem.attribs.src.includes("easternhills"))
+        imageUrl = `https://www.wanakaairport.com/${elem.attribs.src}`;
+    });
+
+    return imageUrl;
+  } catch (error) {
+    throw error;
+  }
+};
