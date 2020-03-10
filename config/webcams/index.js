37478d037a41702e9f7231410a2693a9e4fe17e4
Matt Chapman <matt@mttchpmn.com>
2020-03-10 15:49:12 +1300

    Implement Tamaki Webcams

--- config/webcams/index.js
+++ config/webcams/index.js
@@ -3,11 +3,13 @@ const clyde = require("./clyde");
 const fiords = require("./fiords");
 const gore = require("./gore");
 const windward = require("./windward");
+const tamaki = require("./tamaki");
 
 module.exports = {
   alps,
   clyde,
   fiords,
   gore,
+  tamaki,
   windward
 };
---
+++ config/webcams/tamaki.js
@@ -0,0 +1,161 @@
+module.exports = [
+  {
+    title: "Grey Lynn",
+    code: "ta_greyLynn",
+    desc: "Situated at Grey Lynn looking NE towards Sky Tower",
+    originUrl: "http://greylynnweather.net/cam.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Orewa Beach E",
+    code: "ta_orewaBeachE",
+    desc: "Situated at Orewa Beach looking offshore to the East",
+    originUrl: "http://www.windsurf.co.nz/webcams/orewa.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+
+  {
+    title: "Orewa Beach S",
+    code: "ta_orewaBeachS",
+    desc: "Situated at Orewa Beach looking S",
+    originUrl: "http://www.windsurf.co.nz/webcams/orewa2.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Manly Beach",
+    code: "ta_manlyBeach",
+    desc: "Situated at Manly Beach at Whangaparaoa, looking N",
+    originUrl: "http://www.windsurf.co.nz/webcams/manly_sm.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Point Chevalier",
+    code: "ta_pointChevalier",
+    desc: "Situated at Point Chevalier",
+    originUrl: "http://www.windsurf.co.nz/webcams/ptchev/ptchev.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Takapuna Beach NE",
+    code: "ta_takapunaBeachNE",
+    desc: "Situated at Takapuna Beach looking NE towards Great Barrier Island",
+    originUrl: "http://www.windsurf.co.nz/webcams/takapuna.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Takapuna Beach SE",
+    code: "ta_takapunaBeachSE",
+    desc: "Situated at Takapuna Beach looking SE towards Rangitoto",
+    originUrl: "http://www.takapunabeach.com/netcam.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Coastal Walkway",
+    code: "ta_coastalWalkway",
+    desc:
+      "Situated at the Coastal Walkway at Fletcher Bay on the Coromandel Peninsula looking NW",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/t8144c72ac0w558.jpg?width=720&height=405&hide=1",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Auckland City",
+    code: "ta_aucklandCity",
+    desc: "Situated in the city looking E over the CBD",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/t64684e222bw209.jpg?width=720&height=405&hide=1",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Viaduct Harbor",
+    code: "ta_viaductHarbor",
+    desc: "Situated in the CBD looking W over Viaduct Harbor",
+    originUrl:
+      "https://encounter.snapithd.com/static_new/tb5635fw16.jpg?width=720&height=405&hide=1",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Goat Island",
+    code: "ta_goatIsland",
+    desc: "Situated at Goat Island looking offshore to the East",
+    originUrl: "https://sensorable.io/goat-island-webcam/",
+    static: false,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "Shoal Bay",
+    code: "ta_shoalBay",
+    desc: "Situated at Bayswater looking across Shoal Bay",
+    originUrl: "http://www.windsurf.co.nz/webcams/bayswater.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "North Shore Aero Club W",
+    code: "ta_northShoreAeroClubW",
+    desc: "Situated at North Shore Aero Club in Dairy Flat, looking West",
+    originUrl: "http://weather.nsac.co.nz/cams/orig_west.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  },
+  {
+    title: "North Shore Aero Club E",
+    code: "ta_northShoreAeroClubE",
+    desc: "Situated at North Shore Aero Club in Dairy Flat, looking West",
+    originUrl: "http://weather.nsac.co.nz/cams/orig_east.jpg",
+    static: true,
+    area: "tamaki",
+    areaCode: "ta",
+    region: "auckland",
+    zone: "auckland"
+  }
+];
